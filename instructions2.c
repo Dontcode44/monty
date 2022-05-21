86179fb088e5f3bf3473a166c670a763c0f3ae55
taret92 <ivancho92.10@hotmail.com>
2022-05-21 17:13:36 -0500

    adding add

--- instructions2.c
+++ instructions2.c
@@ -19,6 +19,23 @@ void swap(stack_t **head, unsigned int line_number)
 	(*head)->n = (*head)->next->n;
 	(*head)->next->n = current;
 }
+/**
+ * add - add nodes
+ *
+ * @head: header of the stack
+ * @line_number: number of lines
+ */
+void add(stack_t **head, unsigned int line_number)
+{
+	if (!head || !*head || !(*head)->next)
+	{
+		fprintf(stderr, "L%d: can't %s, stack too short\n", line_number, "add");
+		free_stack(*head), fclose(fileopen);
+		exit(EXIT_FAILURE);
+	}
+	(*head)->next->n = (*head)->n + (*head)->next->n;
+	pop(head, line_number);
+}
 
 /**
  * sub - Substract the value of two nodes.
